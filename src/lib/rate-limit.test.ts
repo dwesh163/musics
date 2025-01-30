0d4ff564de30865196340c8884167e38b51049fd
Rignchen <rinchendemort@gmail.com>
2025-01-30 11:30:36 +0100

    test: ensure rate-limit works

---
+++ src/lib/rate-limit.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from 'vitest';
+import rateLimit from './rate-limit';
+
+describe('rateLimit', () => {
+	it('shouldn allow requests below the limit but not above', () => {
+		const limiter = rateLimit({ uniqueTokenPerInterval: 2, interval: 1000 });
+		expect(limiter.check(2, 'test').isRateLimited).toBe(false);
+		expect(limiter.check(2, 'test').isRateLimited).toBe(false);
+		expect(limiter.check(2, 'test').isRateLimited).toBe(true);
+	});
+	it('should reset after the interval', async () => {
+		const limiter = rateLimit({ uniqueTokenPerInterval: 2, interval: 10 });
+		limiter.check(2, 'test').isRateLimited;
+		limiter.check(2, 'test').isRateLimited;
+		await new Promise((resolve) => setTimeout(resolve, 10));
+		expect(limiter.check(2, 'test').isRateLimited).toBe(false);
+	});
+	it('should return the correct headers', () => {
+		const limiter = rateLimit({ uniqueTokenPerInterval: 2, interval: 1000 });
+		expect(limiter.check(2, 'test').headers).toEqual({
+			'X-RateLimit-Limit': '2',
+			'X-RateLimit-Remaining': '1',
+		});
+	});
+});
