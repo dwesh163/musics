19bac3dd7e24f0cf7d1ce3774292e45747b425c6
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-04 22:21:49 +0100

    feat: add LeftSidebar and RightSidebar components to RootLayout for improved layout structure

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from 'react';
 import React from 'react';
 import { getUser } from '@/lib/auth';
 import { redirect } from 'next/navigation';
+import LeftSidebar from '@/components/left-sidebar';
+import RightSidebar from '@/components/right-sidebar';
 import PlayerBar from '@/components/player-bar';
 import { PlaybackProvider } from '../playback-context';
 
@@ -13,7 +15,8 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
 		<React.Fragment>
 			<PlaybackProvider>
 				<div className="flex">
-	return <main>{children}</main>;
+					<LeftSidebar user={user} />
+					<main className="flex-1 h-[calc(100svh-6.5rem)]">{children}</main>
 					<RightSidebar />
 				</div>
 				<PlayerBar />
