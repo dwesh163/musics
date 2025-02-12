09047959f515272408e0992a90a85c13966977f9
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-12 20:18:33 +0100

    feat: integrate PlaylistProvider to manage playlists and favourites in RootLayout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -6,20 +6,27 @@ import LeftSidebar from '@/components/left-sidebar';
 import RightSidebar from '@/components/right-sidebar';
 import PlayerBar from '@/components/player-bar';
 import { PlaybackProvider } from '../playback-context';
+import { getFavouritePlaylist, getPlaylists } from '@/lib/playlist';
+import { PlaylistProvider } from '../playlists-context';
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
 	const user = await getUser();
 	if (!user) redirect('/login');
 
+	const playlists = await getPlaylists();
+	const favourites = await getFavouritePlaylist();
+
 	return (
 		<React.Fragment>
 			<PlaybackProvider>
-				<div className="flex">
-					<LeftSidebar user={user} />
-					<main className="flex-1 h-[calc(100svh-6.5rem)]">{children}</main>
-					<RightSidebar />
-				</div>
-				<PlayerBar />
+				<PlaylistProvider playlists={playlists || []} favourites={favourites?.tracks.map((track) => track.id) || []}>
+					<div className="flex">
+						<LeftSidebar user={user} playlists={playlists} />
+						<main className="flex-1 h-[calc(100svh-6rem)]">{children}</main>
+						<RightSidebar />
+					</div>
+					<PlayerBar />
+				</PlaylistProvider>
 			</PlaybackProvider>
 		</React.Fragment>
 	);
