dd3a3190ea1e3b3df7126b887905abec540e8558
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-12 20:20:36 +0100

    feat: implement API routes for adding and removing tracks from user's favourite playlist

---
+++ src/app/api/favourites/route.ts
@@ -0,0 +1,58 @@
+import { checkAccreditation, getUser } from '@/lib/auth';
+import { addTrackToPlaylist, removeTrackFromPlaylist } from '@/lib/playlist';
+import { IPlaylist, PlaylistModel } from '@/models/Playlist';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest, context: any) {
+	try {
+		const { trackId } = await req.json();
+
+		const user = await getUser();
+
+		if (!user) {
+			return NextResponse.json({ message: 'Not authenticated.' }, { status: 401 });
+		}
+
+		if (!(await checkAccreditation('app:access'))) {
+			return NextResponse.json({ message: 'Not authorized.' }, { status: 403 });
+		}
+
+		const favouritePlaylist = await PlaylistModel.findOne<IPlaylist>({ _id: user.favouritePlaylist });
+		if (!favouritePlaylist) {
+			return NextResponse.json({ message: 'Favourite playlist not found' }, { status: 404 });
+		}
+
+		const playlist = await addTrackToPlaylist(favouritePlaylist.id, trackId);
+		return NextResponse.json({ message: playlist.message }, { status: playlist.status });
+	} catch (error) {
+		console.error('Failed to add track to favourite:', error);
+		return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+	}
+}
+
+export async function DELETE(req: NextRequest, context: any) {
+	try {
+		const { trackId } = await req.json();
+
+		const user = await getUser();
+
+		if (!user) {
+			return NextResponse.json({ message: 'Not authenticated.' }, { status: 401 });
+		}
+
+		if (!(await checkAccreditation('app:access'))) {
+			return NextResponse.json({ message: 'Not authorized.' }, { status: 403 });
+		}
+
+		const favouritePlaylist = await PlaylistModel.findOne<IPlaylist>({ _id: user.favouritePlaylist });
+		if (!favouritePlaylist) {
+			return NextResponse.json({ message: 'Favourite playlist not found' }, { status: 404 });
+		}
+
+		const playlist = await removeTrackFromPlaylist(favouritePlaylist.id, trackId);
+		return NextResponse.json({ message: playlist.message }, { status: playlist.status });
+	} catch (error) {
+		console.error('Failed to remove track from favourite:', error);
+		return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+	}
+}
