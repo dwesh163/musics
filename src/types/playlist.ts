e73ba7d101a1b0ed8bfd3404fc50888b8026bd18
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-12 20:21:07 +0100

    feat: add types for Playlists and Playlist to enhance type safety

---
+++ src/types/playlist.ts
@@ -0,0 +1,19 @@
+import { SimplifiedTrack } from './track';
+
+export type PlaylistsType = {
+	id: string;
+	name: string;
+	createAt: Date;
+	length: number;
+	duration: number;
+	images: string[];
+};
+
+export type PlaylistType = {
+	id: string;
+	name: string;
+	images: string[];
+	length: number;
+	tracks: SimplifiedTrack[];
+	createAt: Date;
+};
