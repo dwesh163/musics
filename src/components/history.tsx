821adcca144488dc99a673a52704b078d4b659ca
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-12 17:03:43 +0100

    fix: adjust ScrollArea height calculation in History component

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -5,7 +5,7 @@ import { TrackItem } from './TrackItem';
 
 export function History({ history }: { history: SimplifiedTrack[] }) {
 	return (
-		<ScrollArea className="w-full h-[calc(100%-5rem)]">
+		<ScrollArea className="w-full h-[calc(100%-6rem)]">
 			<div className="space-y-2">
 				{history.map((track, index) => (
 					<TrackItem key={track.id + index} track={track} index={index} config={{ index: true, image: true, name: true, artists: true, album: true, play: true }} />
