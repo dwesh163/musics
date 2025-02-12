ad0fa1ec513d83a52773d35598e6364a5ea46ef2
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-12 17:01:28 +0100

    fix: enhance error handling layout in HistoryPage component

--- src/app/(root)/history/page.tsx
+++ src/app/(root)/history/page.tsx
@@ -9,7 +9,12 @@ export default async function HistoryPage() {
 		const history = await getHistory();
 
 		if (!history) {
-			return <Error text="Something went wrong" subText="We couldn't get your search. Please try again later" Icon={AlertCircle} color="text-red-500" />;
+			return (
+				<div className="w-full h-full sm:px-6 px-3">
+					<h1 className="sm:text-2xl text-xl sm:font-bold font-semibold sm:my-6 my-2">History</h1>
+					<Error text="Something went wrong" subText="We couldn't get your search. Please try again later" Icon={AlertCircle} color="text-red-500" />;
+				</div>
+			);
 		}
 
 		return (
