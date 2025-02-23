79ed1af66eabb0153ff41b455ddfe3eaadf18c96
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-02-23 17:39:41 +0100

    refactor: update playlist track handling to include addedAt timestamp and improve data retrieval

--- src/lib/playlist.ts
+++ src/lib/playlist.ts
@@ -58,40 +58,82 @@ export async function getPlaylist(playlistId: string): Promise<PlaylistType | nu
 		const user = await getUser();
 		if (!user) return null;
 
-		const playlist = await PlaylistModel.findOne({ id: playlistId, userId: user.id }).populate({
-			path: 'tracks',
-			populate: [{ path: 'artistsId' }, { path: 'albumId' }],
+		const playlist = await PlaylistModel.findOne<IPlaylist>({
+			id: playlistId,
+			userId: user.id,
 		});
+
 		if (!playlist) return null;
 
-		const trackCount = playlist.tracks.length;
-		const imageIndices = [0.2, 0.4, 0.6, 0.8].map((fraction) => Math.floor(trackCount * fraction)).filter((index) => index < trackCount);
+		const trackIds = playlist.tracks.map((t) => t.id);
+
+		const tracksData = await TrackModel.aggregate([
+			{
+				$match: {
+					_id: { $in: trackIds },
+				},
+			},
+			{
+				$lookup: {
+					from: 'artists',
+					localField: 'artistsId',
+					foreignField: '_id',
+					as: 'artists',
+				},
+			},
+			{
+				$lookup: {
+					from: 'albums',
+					localField: 'albumId',
+					foreignField: '_id',
+					as: 'album',
+				},
+			},
+			{
+				$unwind: '$album',
+			},
+		]).exec();
+
+		const trackAddedDates = new Map(playlist.tracks.map((t) => [t.id.toString(), t.addedAt]));
+
+		const tracks = tracksData.map((track) => ({
+			id: track.id,
+			name: track.name,
+			artists: track.artists.map((artist: IArtist) => ({
+				id: artist.id,
+				name: artist.name,
+			})),
+			album: {
+				id: track.album.id,
+				name: track.album.name,
+				image: track.album.imageUrl,
+			},
+			duration: track.duration,
+			images: track.images.map((image: { url: string; width: number; height: number }) => ({
+				url: image.url,
+				width: image.width,
+				height: image.height,
+			})),
+			addedAt: trackAddedDates.get(track._id.toString())!,
+		}));
+
+		const trackCount = tracks.length;
+		const images =
+			trackCount >= 4
+				? [0.2, 0.4, 0.6, 0.8]
+						.map((fraction) => Math.floor(trackCount * fraction))
+						.filter((index) => index < trackCount)
+						.map((index) => tracks[index]?.images?.[0]?.url)
+						.filter(Boolean)
+				: tracks.map((track) => track.images[0]?.url).filter(Boolean);
 
 		return {
 			id: playlist.id,
 			name: playlist.name,
 			length: trackCount,
 			createAt: playlist.createAt,
-			images: trackCount >= 4 ? imageIndices.map((index) => playlist.tracks[index]?.images?.[0]?.url).filter(Boolean) : playlist.tracks.map((track: ITrack) => track.images[0]?.url),
-			tracks: playlist.tracks.map((track: any) => ({
-				id: track.id,
-				name: track.name,
-				artists: track.artistsId.map((artist: IArtist) => ({
-					id: artist.id,
-					name: artist.name,
-				})),
-				album: {
-					id: track.albumId.id,
-					name: track.albumId.name,
-					image: track.albumId.imageUrl,
-				},
-				duration: track.duration,
-				images: track.images.map((image: any) => ({
-					url: image.url,
-					width: image.width,
-					height: image.height,
-				})),
-			})),
+			images,
+			tracks,
 		};
 	} catch (error) {
 		console.error('Failed to get a playlist:', error);
@@ -104,20 +146,22 @@ export async function getPlaylists(): Promise<PlaylistsType[] | null> {
 		const user = await getUser();
 		if (!user) return null;
 
-		const playlists = await PlaylistModel.find<IPlaylist>({ userId: user.id }).populate<{ tracks: ITrack[] }>('tracks').exec();
+		const playlists = await PlaylistModel.find({ userId: user.id }).populate<{ tracks: { id: ITrack }[] }>('tracks.id').exec();
 
 		return playlists
-			.filter((playlist) => user.favouritePlaylist.toString() != playlist._id.toString())
+			.filter((playlist) => user.favouritePlaylist?.toString() !== playlist._id.toString())
 			.map((playlist) => {
-				const trackCount = playlist.tracks.length;
+				const populatedTracks = playlist.tracks.map((track) => track.id);
+				const trackCount = populatedTracks.length;
 				const imageIndices = [0.2, 0.4, 0.6, 0.8].map((fraction) => Math.floor(trackCount * fraction)).filter((index) => index < trackCount);
+
 				return {
 					id: playlist.id,
 					name: playlist.name,
 					length: trackCount,
-					duration: playlist.tracks.reduce((acc, track) => acc + track.duration, 0),
+					duration: populatedTracks.reduce((acc, track) => acc + track.duration, 0),
 					createAt: playlist.createAt,
-					images: trackCount >= 4 ? imageIndices.map((index) => playlist.tracks[index]?.images?.[0]?.url).filter(Boolean) : playlist.tracks.map((track) => track.images[0]?.url),
+					images: trackCount >= 4 ? imageIndices.map((index) => populatedTracks[index]?.images?.[0]?.url).filter(Boolean) : populatedTracks.map((track) => track.images?.[0]?.url).filter(Boolean),
 				};
 			});
 	} catch (error) {
@@ -137,14 +181,24 @@ export async function addTrackToPlaylist(playlistId: string, trackId: string): P
 			if (!track) return { message: 'Failed to retrieve track metadata', status: 500 };
 		}
 
-		const playlist = await PlaylistModel.findOne({ id: playlistId });
+		const playlist = await PlaylistModel.findOne<IPlaylist>({ id: playlistId });
 		if (!playlist) return { message: 'Playlist not found', status: 404 };
 
-		if (playlist.tracks.includes(track._id)) {
+		if (playlist.tracks.find((t) => t.id.toString() === track._id.toString())) {
 			return { message: 'Track already exists in the playlist', status: 200 };
 		}
 
-		await PlaylistModel.updateOne({ id: playlistId }, { $addToSet: { tracks: track._id } });
+		await PlaylistModel.updateOne(
+			{ id: playlistId },
+			{
+				$addToSet: {
+					tracks: {
+						id: track._id,
+						addedAt: new Date(),
+					},
+				},
+			}
+		);
 
 		return { message: 'Track added to playlist successfully', status: 200 };
 	} catch (error) {
@@ -158,17 +212,17 @@ export async function addTrackToPlaylist(playlistId: string, trackId: string): P
 
 export async function removeTrackFromPlaylist(playlistId: string, trackId: string): Promise<ErrorType> {
 	try {
-		const playlist = await PlaylistModel.findOne({ id: playlistId });
+		const playlist = await PlaylistModel.findOne<IPlaylist>({ id: playlistId });
 		if (!playlist) return { message: 'Playlist not found', status: 404 };
 
 		const track = await TrackModel.findOne({ id: trackId });
 		if (!track) return { message: 'Track not found', status: 404 };
 
-		if (!playlist.tracks.includes(track._id)) {
+		if (!playlist.tracks.find((t) => t.id.toString() === track._id.toString())) {
 			return { message: 'Track not found in the playlist', status: 404 };
 		}
 
-		await PlaylistModel.updateOne({ id: playlistId }, { $pull: { tracks: track._id } });
+		await PlaylistModel.updateOne({ id: playlistId }, { $pull: { tracks: { id: track._id } } });
 
 		return { message: 'Track removed from playlist successfully', status: 200 };
 	} catch (error) {
